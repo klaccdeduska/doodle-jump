42af9ec21f5e8a3336c0fd7b83a9464a81afc20c
klaccdeduska <burov1808@gmail.com>
2024-11-08 15:14:10 +0200

    Update script.js
    scjoi

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d")
-const playerImage = new Image();
-playerImage.src = "doodler-guy.png";
-const platformImage = new Image();
-platformImage.src = "platform.png"
+const ctx = canvas.getContext("2d");
+const parentEI = document.getElementById("parentEI");
 
+
+var playerImage = document.createElement('img') ;
+playerImage.src = "doodler-guy.png";
+playerImage.alt = "doodler guy";
+parentEI.appendChild(img);
+var platformImage = document.createElement('img');
+platformImage.src = "platform.png";
+platformImage.alt = "platform"
 
 let player = {
     x: canvas.width / 2,
@@ -25,23 +30,16 @@ function createPlatform(x, y) {
 }
 
 function initPlatforms() {
-    for (let i = 0; i <
-        platformCount; 1++) {
-         let x = Math.random() *
-            (canvaWight - platformWight);
-         let y = 1 * (canvasHeght / 
-       platformCount);
-       platforms.push(createPlaform(x,
-        y));
-
+    for (let i = 0; i < platformCount; i++) {
+        let x = Math.random() * (canvas.width - platformWidth);
+        let y = i * (canvas.height / platformCount);
+        platforms.push(createPlatform(x, y));
     }
 }
 
 function drawplatforms() {
     platforms.forEach(platform => {
-       ctx.drawImage(platformImage,
-    platform.x , platform.y,
-    platform.width, platform.height) 
+       ctx.drawImage(platformImage, platform.x, platform.y, platform.width, platform.height) 
     });
 };
 
@@ -49,3 +47,50 @@ platformImage.onlload = function() {
     initPlatforms();
     update();
 };
+
+let x = 100;
+let y = 100;
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.y += player.speedY;
+    ctx.drawImage(playerImage, x, y, 50, 50);
+    drawplatforms();
+    x += 1;
+    y += 1;
+    requestAnimationFrame(update);
+}
+  
+playerImage.onload = function() {
+    initPlatforms();
+    update();
+}
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        y -= 5;
+        break;
+      case "ArrowDown":
+        y += 5;
+        break;
+      case "Arrowleft":
+        x -= 5;
+        break;
+      case "ArrowRight":
+        x += 5;
+        break;
+    }   
+});
+
+let score = 0;
+
+function increaseScore(points) {
+    score += points;
+    updateScoreDisplay();
+}
+
+function updateScoreDisplay() {
+        const scoreElement = document.getElementById("score");
+        scoreElement.textContent = `score> ${score} `;    
+}
